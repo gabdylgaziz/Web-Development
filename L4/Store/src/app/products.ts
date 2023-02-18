2db97d51f921edd4a1df0b9d3fac1431394a6317
gabdylgaziz <g_zhagypar@kbtu.kz>
2023-02-18 19:06:45 +0600

    will complete Angular project from tutorial

---
+++ L4/Store/src/app/products.ts
@@ -0,0 +1,40 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Galaxy A13',
+    price: 87421,
+    image: '/assets/a13.jpg',
+    description: 'A large phone with one of the best screens',
+    rating: 5
+  },
+  {
+    id: 1,
+    name: 'Galaxy A13',
+    price: 87421,
+    image: '/assets/a13.jpg',
+    description: 'A large phone with one of the best screens',
+    rating: 5
+  },
+  {
+    id: 1,
+    name: 'Galaxy A13',
+    price: 87421,
+    image: '/assets/a13.jpg',
+    description: 'A large phone with one of the best screens',
+    rating: 5
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
