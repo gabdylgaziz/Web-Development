0bd4190bdd64585f094ac39c98b7d59ace828412
gabdylgaziz <g_zhagypar@kbtu.kz>
2023-04-05 21:05:39 +0600

    added companies list view

--- Lab9/hhback/views.py
+++ Lab9/hhback/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Company, Vacancy
+from django.http import JsonResponse
 
 # Create your views here.
+
+def Companies(request):
+    companies = Company.objects.all()
+    data = {
+        'companies' : list(companies.values())
+    }
+    return JsonResponse(data, status=200)
