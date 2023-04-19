52b84221502feafd42215a16893380797bab4246
gabdylgaziz <g_zhagypar@kbtu.kz>
2023-04-19 21:48:31 +0600

    added all crud methods

--- Lab10/hhfront/hhfront/src/app/companies/companies.component.ts
+++ Lab10/hhfront/hhfront/src/app/companies/companies.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Company } from "../VacancyTemplate";
+import { CompanyService } from "../vacancy.service";
 
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
   styleUrls: ['./companies.component.css']
 })
-export class CompaniesComponent {
+export class CompaniesComponent implements OnInit {
+  companies: Company[];
+  loaded: boolean;
+  newCompany: Company;
+  constructor(private companyService: CompanyService) {
+    this.companies = [];
+    this.loaded = true;
+    this.newCompany = {} as Company;
+  }
 
+  getCompanies() {
+    this.loaded = false;
+    this.companyService.getCompanies().subscribe( (companies) => {
+      this.companies = companies;
+      this.loaded = true;
+    });
+  }
+
+  
+
+  deleteCompany(id : number) {
+    this.companyService.deleteCompany(id).subscribe((response) => {
+      this.companies = this.companies.filter(a => a.id != id);
+    })
+  }
+
+  createCompany() {
+    this.loaded = false;
+  }
+  
+  ngOnInit(): void {
+    this.getCompanies();
+  }
 }
